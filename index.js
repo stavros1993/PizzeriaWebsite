92c277269fbbce642c50814e8bc878ae20ba1419
unknown <stavrosrchp@gmail.com>
2022-08-27 02:20:56 +0300

    Project done

--- index.js
+++ index.js
@@ -146,3 +146,23 @@ userInputFields.forEach((curInputField) => { //For each user input field, we add
     curInputField.nextElementSibling.innerText = ""; //When user clicks on the respective text field, the error field below it disappears
   });
 });
+
+//Get the button:
+scrollToTopButton = document.getElementById("scrollToTopButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
