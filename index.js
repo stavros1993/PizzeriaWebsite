c856c343224d792d368e532512b7d0f6b39515d5
unknown <stavrosrchp@gmail.com>
2022-08-26 15:17:19 +0300

    Many updates

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-
+//Default menu when user visits the page
 $(".food-card").addClass("hide-menu");
 $(".appetizers").addClass("show-menu");
 $("#appetizer-btn").addClass("chosen-menu");
+//
+
+//email input check (newletter form)
 
 function showMenu(x, btnId) {
 
@@ -45,5 +48,44 @@ function showMenu(x, btnId) {
   }
 }
 
-//spaghetti
-//desserts
+
+let email = document.getElementById('email-input');
+const form = document.getElementById('newsletter-form');
+const errorElement = document.getElementById("email-error-field");
+const successElement = document.getElementById("email-success-field");
+
+form.addEventListener("click", (e) => {
+
+  if (email.value === '' || email.value == null) {
+    errorElement.innerText = "Please enter your email.";
+    successElement.innerText = "";
+    e.preventDefault();
+
+  } else if (email.value.length < 6) {
+    errorElement.innerText = "Email must be longer than 6 characters.";
+    successElement.innerText = "";
+    e.preventDefault();
+
+  } else if (email.value.length > 40) {
+    errorElement.innerText = "Email can't be that large.";
+    successElement.innerText = "";
+    e.preventDefault();
+
+  } else if (email.value.split("@").length - 1 > 1) {
+    errorElement.innerText = "Email can only include one @";
+    successElement.innerText = "";
+    e.preventDefault();
+
+  } else if (email.value.split("@").length - 1 === 0) {
+    errorElement.innerText = "Email must include one @";
+    successElement.innerText = "";
+    e.preventDefault();
+
+  } else {
+    errorElement.innerText = "";
+    successElement.innerText = "Valid Email! Redirecting...";
+    $("body").addClass("newsletter-success");
+
+  }
+
+})
